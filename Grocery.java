0bd6b055162b73f493bd2ca88a5ab95fc09b1474
Rylan Sedlacek <rsedlace@mail.umw.edu>
2024-02-09 23:08:56 -0500

    worked on Grocery Store

--- Grocery.java
+++ Grocery.java
@@ -1,10 +1,44 @@
+import java.util.*;
 class Grocery {
-    public static void main(String args[]) {
-
-
-
+    
+    public boolean itemLookup(String itemA) {
+        if (Store.getItem == true;) {
+            return true;
+        } else { return false; } 
+    }
         
+    private void employee() {
+        
+    }
 
+
+    public static void main(String args[]) {
+        Scanner stdin = new Scanner(System.in);
+        boolean notFour = false;
+
+
+        while (!notFour) {
+            System.out.println(" ");
+            System.out.println("1 - Item Lookup");
+            System.out.println("2 - Compare Items");
+            System.out.println("3 - Employee");
+            System.out.println("4 - Exit");
+            System.out.print("Enter choice: ");
+             int choice = stdin.nextInt();
+
+                 if (choice == 1) {
+
+                 } else if (choice == 2) {
+
+                 } else if (choice == 3) {
+
+                 } else if (choice == 4) {
+                    System.exit(1);
+                 }
+        }
+
+
+    
 
 
         
